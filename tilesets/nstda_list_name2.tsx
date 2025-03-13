61ea8eb1538a920d748a9d1520f33b1ff7926cfd
Phubet Book <phubet.forwork@gmail.com>
2025-03-13 16:46:39 +0700

    update

--- tilesets/nstda_list_name2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="nstda_list_name2" tilewidth="32" tileheight="32" tilecount="60" columns="10">
- <image source="nstda_list_name2.png" width="320" height="192"/>
-</tileset>
